4c0c756002c3fd8f40b1c0e684d54e6bd46ca392
redyamsirisha <73577330+redyamsirisha@users.noreply.github.com>
2025-01-20 12:54:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Import routes from other-files.js
+const otherRoutes = require('./other-files');
+
+// Use the imported routes
+app.use('/api', otherRoutes);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
